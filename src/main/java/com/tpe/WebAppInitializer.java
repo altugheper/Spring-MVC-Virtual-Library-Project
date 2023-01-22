328b9c0f123bc41b7861506e51b41605f55bbe0d
Altug Heper <altugheper@gmail.com>
2023-01-22 01:06:14 +0100

    22/01/23

--- src/main/java/com/tpe/WebAppInitializer.java
+++ src/main/java/com/tpe/WebAppInitializer.java
@@ -7,17 +7,21 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     @Override
     // For DB and Hibernate
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{
+                RootContextConfig.class
+        };
     }
 
     @Override
     // MVC config settings
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{
+                WebMvcConfig.class
+        };
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
     }
 }
